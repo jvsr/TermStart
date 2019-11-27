9bc8ec810afbb44e29b0d7459fbd400056bfaa51
jvsr <jellevisser00@gmail.com>
2019-11-27 22:28:37 +0100

    General improvements to working of version control

--- .version_control.bash
+++ .version_control.bash
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-export TERMSTART_DIR="$HOME/TermStart"
-
-git fetch --quiet
+git fetch origin master --quiet
 masterHash=$(git rev-parse "master")
 originHash=$(git rev-parse "origin/master")
 
 if [[ "$masterHash" != "$originHash" ]]; then
 	while true; do
-		read -p "New update available. Would you like to update? (yes/no): "
+		read -p "A new update is available. Would you like to update now? (yes/no): "
 		if [[ ${REPLY} == "yes" ]]; then
 			curBranch=$(git -C $TERMSTART_DIR branch | grep \* | cut -d ' ' -f2)
 			if [[ $curBranch == "master" ]]; then
 				git -C $TERMSTART_DIR pull --quiet
 			else
 				echo "Please switch to the master branch first before updating."
+				exit ;
 			fi
 			unset curBranch
 			break ;
 		elif [[ ${REPLY} == "no" ]]; then
 			break ;
 		fi
+		unset REPLY
 	done
 fi
 
