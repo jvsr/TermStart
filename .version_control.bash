6a9e6a420db6cb7e279be130521b7a0d9cf0debf
jvsr <jvisser@student.codam.nl>
2019-11-27 19:11:40 +0100

    Testing commit for version control (#1)
    Version Control:
    * Adding basic version control

---
+++ .version_control.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export TERMSTART_DIR="$HOME/TermStart"
+
+masterHash=$(git rev-parse "master")
+originHash=$(git rev-parse "origin/version-control")
+
+if [[ "$masterHash" != "$originHash" ]]; then
+	while true; do
+		read -p "New update available. Would you like to update? (yes/no): "
+		if [[ ${REPLY} == "yes" ]]; then
+			git -C $TERMSTART_DIR pull master --quiet
+			break ;
+		elif [[ ${REPLY} == "no" ]]; then
+			break ;
+		fi
+	done
+else
+	echo "$masterHash | $originHash"
+fi
+
+clear
+
+unset masterHash
+unset originHash
