54d60d9029b477baca9abb89c0932de7d0a22c7c
jvsr <jvisser@student.codam.nl>
2019-12-10 20:40:44 +0100

    Credential structure change (#7)
    * Centralizing credentials folder for easier use
    
    * Optimization fix in printing of weather

--- delete_credentials.bash
+++ delete_credentials.bash
@@ -2,5 +2,5 @@
 
 export TERMSTART_DIR="$HOME/TermStart"
 
-rm -rf $TERMSTART_DIR/42_api/credentials
-rm -rf $TERMSTART_DIR/darksky_api/credentials
+rm -rf $TERMSTART_DIR/credentials/DarkSky.cred
+rm -rf $TERMSTART_DIR/credentials/FT.cred
