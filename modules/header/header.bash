7f51e7cfaeae664fef75e16369ebcaffddcd8b2a
jvsr <jellevisser00@gmail.com>
2019-12-10 21:24:09 +0100

    merge conflict

--- modules/header/header.bash
+++ modules/header/header.bash
@@ -1,10 +1,36 @@
 #!/bin/bash
 
-declare -a baseHeader=(
-"██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗"
-"██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝"
-"██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  "
-"██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝  "
-"╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗"
-" ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝"
-)
+set -euo pipefail
+IFS=$'\n\t'
+
+source $TERMSTART_DIR/config/color.conf
+
+source $TERMSTART_DIR/includes/print.bash
+source $TERMSTART_DIR/includes/color.bash
+
+source $TERMSTART_DIR/modules/header/base_header.bash
+source $TERMSTART_DIR/modules/header/name_header.bash
+
+putColorFg $textColor
+
+printBaseHeader () {
+	local counter=0;
+	while [[ counter -lt 6 ]]; do
+		putstr_center ${baseHeader[$counter]} 80
+		putchar '\n'
+		counter=$((counter + 1))
+	done
+}
+printNameHeader () {
+	constructNameHeader
+	local counter=0;
+	while [[ counter -lt 6 ]]; do
+		putstr_center ${nameHeader[$counter]} 80
+		putchar '\n'
+		counter=$((counter + 1))
+	done
+}
+
+printBaseHeader
+printNameHeader
+resetColor
