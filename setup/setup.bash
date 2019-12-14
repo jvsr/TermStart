987ed16e57d777d434e7bd89d329653f73c4c9cb
Jvsr <jvisser@student.codam.nl>
2019-12-14 17:43:12 +0100

    Final stages of configuration, without bug checking or formatting

--- setup/setup.bash
+++ setup/setup.bash
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-source $TERMSTART_DIR/credentials/create.bash
+source $TERMSTART_DIR/config/modulelist.conf
 
-declare -a allModules=(
-	"header"
-	"date"
-	"weather"
-	"ftCoalitionPoints"
-);
-declare -a ftModules=(
-	"ftCoalitionPoints"
-);
-declare -a darkSkyModules=(
-	"weather"
-);
+source $TERMSTART_DIR/credentials/create.bash
 
 promptModule () {
 	local module=$1
@@ -29,28 +18,64 @@ promptModule () {
 	done
 }
 
+disableModules () {
+	local modules=$1
+	
+	for i in "${modules[@]}"; do
+		echo $i"Module=0" >> $TERMSTART_DIR/config/module.conf
+	done
+}
+
 getFtApiKey () {
 	createCredentialsFT
 	for i in "${ftModules[@]}"; do
-		promptModule ${ftModules[i]}
+		promptModule $i
 	done
 }
 
 requestFtApiKey () {
-	echo hi
+	echo "# FT Dependent Modules" >> $TERMSTART_DIR/config/module.conf
 	while true; do
 		clear
 		read -p "Do you have a valid 42 api key? [y/n]: "
 		case ${REPLY} in
 			y) getFtApiKey; break;;
-			n) echo no;;
+			n) disableModules $ftModules; break;;
+		esac
+		unset REPLY
+	done
+}
+
+getDarkSkyApiKey () {
+	createCredentialsDarkSky
+	for i in "${darkSkyModules[@]}"; do
+		promptModule $i
+	done
+}
+
+requestDarkSkyApiKey () {
+	echo "# Dark Sky Dependent Modules" >> $TERMSTART_DIR/config/module.conf
+	while true; do
+		clear
+		read -p "Do you have a valid darksky api key? [y/n]: "
+		case ${REPLY} in
+			y) getDarkSkyApiKey; break;;
+			n) disableModules $darkSkyModules; break;;
 		esac
 		unset REPLY
 	done
 }
 
 configureTermStart () {
+	echo "Welcome to TermStart configuration"
+	read -n 1 -s -r -p "Press any key to continue"
+	clear
+	echo "# Independent Modules" > $TERMSTART_DIR/config/module.conf
+	for i in "${independentModules[@]}"; do
+		promptModule $i
+	done
 	requestFtApiKey
+	requestDarkSkyApiKey
 }
 
 
